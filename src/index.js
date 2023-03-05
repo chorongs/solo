16c43a66d9863f0a44105407dc5a426bdb6322c0
chorongs <charmblues@gmail.com>
2023-03-05 19:05:48 +0900

    1Day

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+import Allrooms from './pages/Allrooms';
+import NewRoom from './pages/NewRoom';
+import NotFound from './pages/NotFound';
+import RoomDetail from './pages/RoomDetail';
+import MyCart from './pages/MyCart';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/rooms', element: <Allrooms /> },
+      {
+        path: '/rooms/new',
+        element: <NewRoom />,
+      },
+      {
+        path: '/rooms/:id',
+        element: <RoomDetail />,
+      },
+      {
+        path: '/carts',
+        element: <MyCart />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
